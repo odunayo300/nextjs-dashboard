4556c81154bfacb4688031aa71fc38dc11718841
odunayo300 <isaacodunayo300@gmail.com>
2025-04-13 10:11:59 -0700

    font error fixed

--- app/ui/Font.ts
+++ app/ui/Font.ts
@@ -2,4 +2,4 @@ import {Inter} from 'next/font/google';
 export const inter = Inter({subsets:['latin']});
 
 import { Lusitana } from 'next/font/google';
-export const lusitana = Lusitana({weight:"700"});
+export const lusitana = Lusitana({ weight: "700", subsets: ['latin'] });
